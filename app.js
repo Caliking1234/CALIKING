d575e244827bb699a66133fe64cd25d39416802c
Piyush <piyyush1219@gmail.com>
2022-02-24 08:43:50 +0530

    commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+let port = 80;
+
+app.get("/", (req,res)=>{
+    res.send("home page");
+})
+
+app.get("/library", (req,res)=>{
+    res.send("library page");
+})
+
+app.get("/activity", (req,res)=>{
+    res.send("activity page");
+})
+
+app.listen(port, ()=>{
+    console.log("app started at port"+port);
+})
